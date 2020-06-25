e270aa413e244ef1405a90529c92e3004e1adb6d
Samael <samael803@gmail.com>
2020-06-25 11:04:46 +0300

    Implement MovieCard behavior

--- src/Components/MovieCards/MovieCards.jsx
+++ src/Components/MovieCards/MovieCards.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
 
 import './MovieCards.css';
 
 export default function MovieCards({ movies }) {
+
+	if (movies.length === 0) {
+		return (
+			<p>No results</p>
+		);
+	}
 	return (
-		<p>MovieCards</p>
+		<Container>
+			<CardDeck>
+				{
+					movies.map((movie) => {
+						return (
+							<Col sm={4}>
+								<Card className="movie-card-item">
+									<Card.Img className="movie-card-pic" variant="top" src={movie.Poster} />
+									<Card.Body>
+										<Card.Title>{movie.Title}</Card.Title>
+										<Card.Text>{movie.Year}</Card.Text>
+									</Card.Body>
+									<Card.Footer>
+										<small className="text-muted">Full details...</small>
+									</Card.Footer>
+								</Card>
+							</Col>
+						);
+					})
+				}
+			</CardDeck>
+		</Container>
 	);
 }
 
