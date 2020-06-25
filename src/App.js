f516c2222472bfd3aed98c3039f1bcba8a732a2c
Samael <samael803@gmail.com>
2020-06-25 08:47:26 +0300

    Set search form layout and basic styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <Container fluid className="app-search-main">
+        <Form>
+          <Form.Row float="center">
+
+            <Col xs={7}>
+              <Form.Control placeholder="Search for movies, series or episodes..." size="lg" />
+            </Col>
+
+            <Col>
+              <Form.Control as="select" size="lg">
+                <option>All</option>
+                <option>Movies</option>
+                <option>Series</option>
+                <option>Episodes</option>
+              </Form.Control>
+            </Col>
+
+            <Col>
+              <Button type="submit" block size="lg">
+                Go!
+              </Button>
+            </Col>
+
+          </Form.Row>
+        </Form>
+      </Container>
+    </header>
   );
 }
-
-export default App;
