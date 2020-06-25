dace85d833d9341809f5f68fac9c59fcaa818433
Samael <samael803@gmail.com>
2020-06-25 11:54:42 +0300

    Render results in MovieCards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import axios from 'axios';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import MovieCards from './Components/MovieCards';
 
 import './App.css';
 
@@ -46,42 +47,52 @@ export default function App() {
   };
 
   return (
-    <header>
-      <Container fluid className="app-search-main">
-        <Form onSubmit={(e) => handleUserSearch(e)}>
-          <Form.Row float="center">
+    <Fragment>
 
-            <Col xs={7}>
-              <Form.Control
-                placeholder="Search for movies, series or episodes..."
-                size="lg"
-                value={titleQuery}
-                onChange={(e) => setTitleQuery(e.currentTarget.value)}
-              />
-            </Col>
+      <header>
+        <Container fluid className="app-search-main">
+          <Form onSubmit={(e) => handleUserSearch(e)}>
+            <Form.Row float="center">
 
-            <Col>
-              <Form.Control as="select"
-                size="lg"
-                value={titleType}
-                onChange={(e) => setTitleType(e.currentTarget.value)}
-              >
-                <option value="all">All</option>
-                <option value="movie">Movies</option>
-                <option value="series">Series</option>
-                <option value="episode">Episodes</option>
-              </Form.Control>
-            </Col>
+              <Col xs={7}>
+                <Form.Control
+                  placeholder="Search for movies, series or episodes..."
+                  size="lg"
+                  value={titleQuery}
+                  onChange={(e) => setTitleQuery(e.currentTarget.value)}
+                />
+              </Col>
 
-            <Col>
-              <Button type="submit" size="lg" block>
-                Go!
-              </Button>
-            </Col>
+              <Col>
+                <Form.Control as="select"
+                  size="lg"
+                  value={titleType}
+                  onChange={(e) => setTitleType(e.currentTarget.value)}
+                >
+                  <option value="all">All</option>
+                  <option value="movie">Movies</option>
+                  <option value="series">Series</option>
+                  <option value="episode">Episodes</option>
+                </Form.Control>
+              </Col>
 
-          </Form.Row>
-        </Form>
-      </Container>
-    </header>
+              <Col>
+                <Button type="submit" size="lg" block>
+                  Go!
+                </Button>
+              </Col>
+
+            </Form.Row>
+          </Form>
+        </Container>
+      </header>
+
+      <section className="main-stage">
+        <Container>
+          <MovieCards movies={movies} alert={alert} />
+        </Container>
+      </section>
+
+    </Fragment>
   );
 }
