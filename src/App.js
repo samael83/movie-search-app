2cafc0a528cce90e18bb3309bf24f108ff863015
Samael <samael803@gmail.com>
2020-06-25 08:56:49 +0300

    Hook search form values to state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
@@ -7,27 +7,47 @@ import Button from 'react-bootstrap/Button';
 import './App.css';
 
 export default function App() {
+
+  const [titleQuery, setTitleQuery] = useState('')
+  const [titleType, setTitleType] = useState('all');
+
+  const handleUserSearch = (e) => {
+    // stop form submitting
+    e.preventDefault(); 
+    console.log(titleQuery);
+    console.log(titleType);
+  };
+
   return (
     <header>
       <Container fluid className="app-search-main">
-        <Form>
+        <Form onSubmit={(e) => handleUserSearch(e)}>
           <Form.Row float="center">
 
             <Col xs={7}>
-              <Form.Control placeholder="Search for movies, series or episodes..." size="lg" />
+              <Form.Control
+                placeholder="Search for movies, series or episodes..."
+                size="lg"
+                value={titleQuery}
+                onChange={(e) => setTitleQuery(e.currentTarget.value)}
+              />
             </Col>
 
             <Col>
-              <Form.Control as="select" size="lg">
-                <option>All</option>
-                <option>Movies</option>
-                <option>Series</option>
-                <option>Episodes</option>
+              <Form.Control as="select"
+                size="lg"
+                value={titleType}
+                onChange={(e) => setTitleType(e.currentTarget.value)}
+              >
+                <option value="all">All</option>
+                <option value="movie">Movies</option>
+                <option value="series">Series</option>
+                <option value="episode">Episodes</option>
               </Form.Control>
             </Col>
 
             <Col>
-              <Button type="submit" block size="lg">
+              <Button type="submit" size="lg" block>
                 Go!
               </Button>
             </Col>
